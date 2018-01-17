fb6ab4a535f3c5997c1d35abd3f44eb31149b3c6
Peter <newtownfam@gmail.com>
2018-01-16 19:42:41 -0500

    Phase 1

--- p1_phase1.c
+++ p1_phase1.c
@@ -1,66 +1,67 @@
 // included libraries
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <errno.h>
 #include <time.h>
 #include <unistd.h>
+#include <sys/wait.h>
+
 
 /* Parent process function
  * keeps track of time and waits on the child process
  * prints out the statistics */
-private static void parentProcess(int startTime)
+void parentProcess(int startTime)
 {
 	// variables for calculating time
 	int endTime;
 	int timeTaken;
+	time_t seconds;
 
 	wait(0); // waits for the child process to be finished
-	endTime = gettimeofday(); // gets the time after the child has finished
+	seconds = time(NULL); // gets the time after the child has finished
 	timeTaken = (endTime - startTime) * 100; // calculates the time in milliseconds
-
 	/* print out statistics */
-	printf("-- Statistics --");
-	printf("\tElapsed Time: %d", timeTaken);
-	printf("\tPage faults: "); // needs implementation
-	printf("\tPage faults (relaimed): "); // needs implementation
+	printf("\n-- Statistics --\n");
+	printf("\tElapsed Time: %d\n", timeTaken);
+	printf("\tPage faults: \n"); // needs implementation
+	printf("\tPage faults (relaimed): \n\n"); // needs implementation
 }
 
 /* Child process function
  * Exec to become the wanted function */
-private static void childProcess(char * args)
+void childProcess(char * option, char ** args)
 {
-	execvp(args[0], args);
+	execvp(option, args);
 }
 
 int main(int argc, int argv[])
 {
 	int returnVal; // hold the value returned by fork
-	String userInput; // hold the option chosen by the user
+	int userInput; // hold the option chosen by the user
 	int startTime; // hold the start time of the child process
 	char * args[2];
+	char * option;
+	args[1] = NULL;
 
 	/* Initial startup title */
-	printf("==== Mid-Day Commander, vO ====");
+	printf("==== Mid-Day Commander, vO ====\n");
 
-	while(true)
+	while(!0)
 	{
 		/* Print out all the options and take input */
-		printf("What's Good Commander? What command would you like to run?");
-		printf("\t0. whoami  : Prints out the result of the whoamicommand");
-		printf("\t1. last    : Prints out the result of the last command");
-		printf("\t2. ls      : Prints out the result of a listing on a user-specified path");
+		printf("What's Good Commander? What command would you like to run?\n");
+		printf("\t0. whoami  : Prints out the result of the whoami command\n");
+		printf("\t1. last    : Prints out the result of the last command\n");
+		printf("\t2. ls      : Prints out the result of a listing on a user-specified path\n");
 		printf("Option? (control C to exit): ");
-		scanf("%s", userInput);
-
-		/* attempting to handle user input error */
-		if(userInput > 2 || userInput <0)
-		{
-			perror("The following error occured: ");
-		}
+		scanf("%d", &userInput);
+		printf("\n");
 
 		/* fork the parent process to create a child */
 		returnVal = fork();
-		startTime = gettimeofday();
+		time_t seconds;
+		startTime = time(NULL);
 
 		/* Run the chlild or parent function depending on the return value */
 		if(returnVal != 0)
@@ -74,16 +75,19 @@ int main(int argc, int argv[])
 			switch(userInput) 
 			{
 				case 0:
-					args[0] = {"whoami", 0};
-					childProcess(args);
+					option = "whoami";
+					args[0] = option;
+					childProcess(option, args);
 					break;
 				case 1:
-					args[0] = {"last", 0};
-					childProcess(args);
+					option = "last";
+					args[0] = option;
+					childProcess(option, args);
 					break;
 				case 2:
-					args[0] = {"ls", 0};
-					childProcess(args);
+					option = "ls";
+					args[0] = option;
+					childProcess(option, args);
 					break;
 				default:
 					printf("Invalid input");
@@ -93,3 +97,4 @@ int main(int argc, int argv[])
 	}
 
 }
+
