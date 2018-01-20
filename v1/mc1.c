0ddc20d268cd6528b19a321e49071687a2857b37
Andrew Morrison <andrew@Andrews-MacBook-Pro.local>
2018-01-19 19:55:18 -0500

    made some changes for phase 2

--- v1/mc1.c
+++ v1/mc1.c
@@ -50,16 +50,42 @@ void parentProcess()
  * Exec to become the wanted function */
 void childProcess(char * option, char ** args)
 {
-	execvp(option, args); // system fcuntion to execute the three commands
+	int rc = execvp(option, args); // system fcuntion to execute the three commands
+	assert(rc==0);
+}
+
+/* Arguments function
+ * Gets user inputted arguments */
+int getArgs(char * argString, char * args)
+{
+	/* User input */
+	scanf("Arguments?: %s", argString);
+
+	/* Check if string is too long */
+	if(argString > 128)
+	{
+		printf("Error. String too long");
+		return -1;
+	}
+
+	/* Store the arguments in args[i] */
+	int i = 0;
+	for(i=1; i<=32; i++)
+	{
+		args[i] = strtok(" ");
+	}
+	args[i+1] = NULL;
+	return 0;
 }
 
 int main(int argc, int argv[])
 {
 	int returnVal; // hold the value returned by fork
 	char userInput; // hold the option chosen by the user
-	char * args[2];
-	char * option;
-	char buff;
+	char * args[34]; // holds the arguments
+	char * option[1024]; // holds the option
+	char * argString[130]; // holds the total argument string
+	char buff; // buff to eat up the enter key
 	args[1] = NULL;
 
 	/* Initial startup title */
@@ -73,7 +99,7 @@ int main(int argc, int argv[])
 		printf("\t1. last    : Prints out the result of the last command\n");
 		printf("\t2. ls      : Prints out the result of a listing on a user-specified path\n");
 		printf("\ta. add command : adds a new command to the menu\n");
-		printf("\tc. change directory :Changes process working directory\n");
+		printf("\tc. change directory : Changes process working directory\n"); // done: needs testing
 		printf("\te. exit : exit midday Commander\n"); // working: exits terminal
 		printf("\tp. pwd : Prints working directory\n");
 		printf("Option? (control C to exit): \n");
@@ -93,62 +119,47 @@ int main(int argc, int argv[])
 		}
 		else
 		{
-			// child process gets a return value equal to 0
+			// child process gets a return value equal to zero
+
+			/* Get arguments if necessary */
+			if(userInput != 'a' && userInput != 'c' && userInput != 'e' && userInput != "p")
+			{
+				returnVal = getArgs(argString, args);
+				assert(returnVal == 0);
+			}
 			switch(userInput) 
 			{
 				case '0':
 					option = "whoami";
-					for(int i; i<strlen(option); i++)
-					{
-						printf("--- %c ---\n", option[i]);
-					}
 					args[0] = option;
 					childProcess(option, args);
 					break;
 				case '1':
 					option = "last";
-					for(int i; i<strlen(option); i++)
-					{
-						printf("--- %c ---\n", option[i]);
-					}
 					args[0] = option;
 					childProcess(option, args);
 					break;
 				case '2':
 					option = "ls";
-					for(int i; i<strlen(option); i++)
-					{
-						printf("--- %c ---\n", option[i]);
-					}
 					args[0] = option;
 					childProcess(option, args);
 					break;
 				case 'a':
 					*option = 'a';
-					for(int i; i<strlen(option); i++)
-					{
-						printf("--- %c ---\n", option[i]);
-					}
 					args[0] = option;
 					childProcess(option, args);
 				case 'c':
 					*option = 'c';
-					for(int i; i<strlen(option); i++)
-					{
-						printf("--- %c ---\n", option[i]);
-					}
-					args[0] = option;
-					childProcess(option, args);
+					scanf("Directory: %s", &option);
+					chdir(option;
 				case 'e':
 					break;
 				case 'p':
 					*option = 'p';
-					for(int i; i<strlen(option); i++)
+					if(getcwd(option, sizeOf(option)) != NULL)
 					{
-						printf("--- %c ---\n", option[i]);
+						// print the cwd
 					}
-					args[0] = option;
-					childProcess(option, args);
 				default:
 					printf("Invalid input\n");
 					break;
