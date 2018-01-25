977a6807252f2b1f8d1eb198795c5beb1b3fcde7
Tim Winters <twinters@wpi.edu>
2018-01-25 15:42:42 -0500

    Here andrew

--- hw1_Morrison_Christakos/mc2.c
+++ hw1_Morrison_Christakos/mc2.c
@@ -18,8 +18,11 @@ struct timeval clockTime; // struct to access gettimeofday
  */
 struct info
 {
+	char *args[32];
+	int numargs;
 	int num;
-	char name[20];
+	int bg;
+	char *name;
 };
 
 struct processes
@@ -35,52 +38,49 @@ struct processes
  * prints out the statistics */
 int parentProcess(struct processes bgProcesses[], int bgPending, int bgRunning, pid_t rv)
 {
-
 	/* Implement background process capability
 	 * step 1. create data structure to keep track of background processes running
 	 * step 2. -r running processes list prints out the processes from step 1
-	 * step 3. change waiting and implement the data structure
+	 * step 3. change waiting and implement the data structure*/
 
 	/* variables for calculating time */
 	long endTime, startTime, elapsedTime; // time variables
 	int pagefaults; // pagefaults
 	int pagefaults_r; // reclaimed pagefaults FIX THIS
-	pid_t lastpid;
 
+	/* find elapsed time */
+	gettimeofday(&clockTime, NULL); // get time before child process starts
+	startTime = (clockTime.tv_sec * 1000) + (clockTime.tv_usec / 1000); // convert to ms
+	
+	wait(NULL);
+	//waitpid(rv, NULL, WNOHANG);
 
-		/* find elapsed time */
-		gettimeofday(&clockTime, NULL); // get time before child process starts
-		startTime = (clockTime.tv_sec * 1000) + (clockTime.tv_usec / 1000); // convert to ms
-		
-		wait(NULL);
-		//waitpid(rv, NULL, WNOHANG);
+	gettimeofday(&clockTime, NULL); // get time after child process ends
 
-		gettimeofday(&clockTime, NULL); // get time after child process ends
+	endTime = (clockTime.tv_sec * 1000) + (clockTime.tv_usec / 1000); // convert to ms
+	elapsedTime = endTime - startTime; // record elapsed time
 
-		endTime = (clockTime.tv_sec * 1000) + (clockTime.tv_usec / 1000); // convert to ms
-		elapsedTime = endTime - startTime; // record elapsed time
+	/* use getrusage function for page faults (not sure if working properly) */
+	getrusage(RUSAGE_CHILDREN, &usage); // RTFMP			
+	pagefaults_r = usage.ru_minflt; // getrusage function to show reclaimed page faults
+	pagefaults = usage.ru_majflt; //shows non-reclaimed page faults
 
-		/* use getrusage function for page faults (not sure if working properly) */
-		getrusage(RUSAGE_CHILDREN, &usage); // RTFMP			
-		pagefaults_r = usage.ru_minflt; // getrusage function to show reclaimed page faults
-		pagefaults = usage.ru_majflt; //shows non-reclaimed page faults
+	/* print out statistics */
+	if(bgPending == 0)
+	{
+		printf("\n--  Background Task Complete --\n");
+	}
+	else
+	{
+		printf("\n-- Task Complete --\n");
+	}
+	printf("\tElapsed Time: %ld milliseconds\n", elapsedTime);
+	printf("\tPage faults: %d\n", pagefaults); // needs implementation
+	printf("\tPage faults (relaimed): %d\n\n", pagefaults_r); // needs implementation
 
-		/* print out statistics */
-		if(bgPending == 0)
-		{
-			printf("\n--  Background Task Complete --\n");
-		}
-		else
-		{
-			printf("\n-- Task Complete --\n");
-		}
-		printf("\tElapsed Time: %ld milliseconds\n", elapsedTime);
-		printf("\tPage faults: %d\n", pagefaults); // needs implementation
-		printf("\tPage faults (relaimed): %d\n\n", pagefaults_r); // needs implementation
+	pagefaults = 0;
 
-		pagefaults = 0;
-
-		return 0;
+	return 0;
 
 }
 
@@ -93,7 +93,7 @@ void childProcess(char * option, char ** args)
 }
 
 /* main */
-int main(int argc, char ** argv[])
+int main(int argc, char ** argv)
 {
 	struct processes bgProcesses[200]; // holds background processes
 
@@ -105,13 +105,17 @@ int main(int argc, char ** argv[])
 	}
 
 	struct info commands[400]; // holds new commands
+	struct info currentCommand;
+        struct info emptyCommand;
+	for(int i = 0; i < 32; i++)
+	{
+		emptyCommand.args[i] = NULL;
+	}
 	//int exit = 0; // hold the option chosen by the user
 	int k = 3; // integer to keep track of user-added commands
 	//FILE *file; // create the ability to read a file
 	//char c; // character used to read input file
 	int bgRunning = 0;
-	int bgTotal;
-	int getLineCount;
 
 	
 	/* Initial startup title */
@@ -120,19 +124,19 @@ int main(int argc, char ** argv[])
 	/* loop until exit is called*/
 	while(1)
 	{
-		char *thing = NULL;
+		fflush(stdout);
+		currentCommand = emptyCommand;
+		char thing[100];
 		char *buffer = NULL;
-		size_t buffSize = 80;
 		int returnVal=0; // hold the value returned by fork
 		char *args[100]; // holds the arguments
 		//char argString[1024]; // holds the total argument string
 		//argString[1023] = 0x0;
-		char buff; // buff to eat up the enter key
-		char * path=NULL;; // holds path for 2.
+		char path[101]; // holds path for 2.
 		char * dir=NULL; // holds directory name for c.
-		char userInput; // holds the char of user input
+		char userInput[100]; // holds the char of user input
 		int current = -1;
-		int kill = 0;
+		char split[100];
 
 		/* Print out all the options and take input */
 		printf("\nWhat's popping Commander? What command would you like to run?\n");
@@ -143,7 +147,7 @@ int main(int argc, char ** argv[])
 		/* Prints out all user-added commands */
 		for(int n=3; n<k; n++)
 		{
-			printf("\t%d. user command: %s\n", commands[n].num, commands[n].name);
+			printf("\t%d. %s\t: user added command\n", commands[n].num, commands[n].name);
 		}
 
 		/* prints out a, c, e, and p commands */
@@ -155,31 +159,34 @@ int main(int argc, char ** argv[])
 		printf("Option? (control C to exit): ");
 
 		/* take input */
-		getLineCount = getline(&thing, &buffSize, stdin);
-		thing[getLineCount - 1] = '\0';
-		userInput = thing[0];
-		
-		if(userInput == 'e')
-			{
-				printf("Peace out, Commander\n");
-				return 0;
-			}
-		/* Get arguments if necessary */
-		if(userInput != 'a' && userInput != 'c' && userInput != 'e' && userInput != 'p' && userInput != 'r')
+		if(fgets(userInput, 100, stdin)==NULL)
 		{
-
-			
+			printf("End of file reached\n");
+			break;
+		}
+		userInput[strlen(userInput)-1] = '\0';
+		printf("User input: %s\n", userInput);
+		
+		if(userInput[0] == 'e')
+		{
+			printf("Peace out, Commander\n");
+			return 0;
+		}
+		/* Get arguments if necessary */
+		if (userInput[0] == '2')
+		{
 			int i = 0;
-
-			if(userInput == '2')
-			{
-				i++;
-			}
 
 			/* User input */
 			printf("Arguments (type N for none): ");
-			getLineCount = getline(&thing, &buffSize, stdin);
-			thing[getLineCount-1] = '\0';
+
+			if(fgets(thing, 100, stdin)==NULL)
+			{
+				printf("End of file reached\n");
+				break;
+			}
+			thing[strlen(thing)-1] = '\0';
+			printf("Args: %s\n", thing);
 			if(strcmp(thing, "N") != 0)
 			{
 				/* Store the arguments in args[i] */
@@ -227,81 +234,116 @@ int main(int argc, char ** argv[])
 				}
 			}
 		}*/
-			// child process gets a return value equal to zero
-			char * option; // holds the option 
-			char option2[1024]; // holds the option if scanf() must be used
-			//int u = atoi(userInput);
+		// child process gets a return value equal to zero
+		char option[100]; // holds the option 
+		//int u = atoi(userInput);
 
-			/* Fill option, args[0], etc. and perform commands */
-			switch(userInput) 
-			{
-				case '0':
-					option = "whoami";
-					args[0] = option;
+		/* Fill option, args[0], etc. and perform commands */
+		switch(userInput[0]) 
+		{
+			case '0':
+				strncpy(option, "whoami", 100);
+				args[0] = option;
+				printf("--Who am I?--\n");
+				break;
+			case '1':
+				strncpy(option, "last", 100);
+				args[0] = option;
+				break;
+			case '2':
+				strncpy(option, "ls", 100);
+				printf("Specify Path: \n");
+				if(fgets(path, 100, stdin) == NULL)
+				{
+					printf("End of file reached\n");
 					break;
-				case '1':
-					option = "last";
-					args[0] = option;
+				}
+				path[strlen(path)-1] = '\0';
+				printf("Your path was: %s\n", path);
+				args[0] = option; // store "ls"
+				for(int i = 0;;i++){
+				if (args[i] == NULL){
+					args[i] = path; // store path name
 					break;
-				case '2':
-					option = "ls";
-					printf("Specify Path: \n");
-					getLineCount = getline(&path, &buffSize, stdin);
-					path[getLineCount-1] = '\0';
-					printf("Your path was: %s\n", path);
-					args[0] = option; // store "ls"
-					args[1] = path; // store path name
-					break;
-				case 'a':
-					printf("Enter function name for %d: \n", k);
+				}
+				}
+				break;
+			case 'a':
+				printf("Enter function name for %d: \n", k);
 
-					/* Save user-added command parameters into commands */
-					getLineCount = getline(&thing, &buffSize, stdin);
-					thing[getLineCount-1] = '\0';
-
-					strcpy(commands[k].name, thing);
-					commands[k].num = k;// update ammount of user-added commands
-					k++;
+				/* Save user-added command parameters into commands */
+				if(fgets(split, 100, stdin) == NULL)
+				{
+					printf("End of file reached\n");
 					break;
-				case 'c':
-					printf("Directory?: \n");
-					getLineCount = getline(&option, &buffSize, stdin);
-					thing[getLineCount-1] = '\0';
-					chdir(option);
+				}
+				split[strlen(split)-1] = '\0';
+				if (split[strlen(split)-1] == '&'){
+					commands[k].bg = 0;
+					split[strlen(split)-1] = '\0';
+				}
+				else
+					commands[k].bg = -1;
+				
+				char* token = strtok(split, " ");
+				commands[k].name = strdup(token);
+				commands[k].args[0] = strdup(token);
+				commands[k].numargs = 1;
+				for(token = strtok(NULL, " "); token!=NULL; token = strtok(NULL, " ")){
+				    commands[k].args[commands[k].numargs] = strdup(token);
+				    commands[k].numargs++;
+				}
+				//strcpy(commands[k].name, thing);
+				printf("Command: %s\n", split); 
+				commands[k].num = k;// update ammount of user-added commands
+				k++;
+				break;
+			case 'c':
+				printf("Directory?: ");
+				if(fgets(option, 100, stdin) == NULL)
+				{
+					printf("End of file reached\n");
 					break;
-				case 'p':
-					if(getcwd(dir, sizeof(dir)) != NULL)
+				}
+				printf("%s\n", option);
+				option[strlen(option)-2] = '\0';
+				chdir(option);
+				printf("ADSFADS");
+				break;
+			case 'p':
+				strncpy(option, "p", 100);
+				args[0] = "pwd";
+				break;
+			case 'r':
+				printf("\n~~~~~~Background boyes~~~~~~~\n");
+				for(int m = 0; m<200; m++)
+				{
+					if(bgProcesses[m].num!=0)
 					{
-						printf("Current Directory: %s\n", dir);
-					}
-					break;
-				case 'r':
-					printf("\n~~~~~~Background boyes~~~~~~~\n");
-					for(int m = 0; m<200; m++)
-					{
-						if(bgProcesses[m].num!=0)
-						{
-							printf("BG Process #: %d, BG PID: %d\n", bgProcesses[m].num, bgProcesses[m].pid);
-						}
-					}
-					break;
-				default: // default will handle user-added commands
-					for(int n=3; n<k; n++)
-					{	
-						/* find the user-added command that matched userInput */
-						if(atoi(&userInput) == commands[n].num)
-						{
-							/* run the necessary command */
-							args[0] = commands[n].name;
-							printf("\ncommands: %s\n", commands[n].name);
-							break;	
-						}
-						
+						printf("BG Process #: %d, BG PID: %d\n", bgProcesses[m].num, bgProcesses[m].pid);
 					}
 				}
+				break;
+			default: // default will handle user-added commands
+				for(int n=3; n<k; n++)
+				{	
+					/* find the user-added command that matched userInput */
+					if(atoi(&(userInput[0])) == commands[n].num)
+					{
+						/* run the necessary command */
+						args[0] = commands[n].name;
+						currentCommand = commands[n];
+						current = currentCommand.bg;
+						printf("\ncommands: %s\n", commands[n].name);
+						break;	
+					}
+					
+				}
+			}
 
-				if(userInput != 'a' && userInput != 'e' &&userInput !='r' && userInput != 'c' && userInput != 'p')
-				{
+		fflush(stdout);
+			if(userInput[0] != 'a' && userInput[0] != 'e' &&userInput[0] !='r' && userInput[0] != 'c')
+			{
 				returnVal = fork();
 				if(returnVal > 0) // parent
 				{
@@ -309,17 +351,13 @@ int main(int argc, char ** argv[])
 					{
 						parentProcess(bgProcesses, current, bgRunning, returnVal);
 					}
-					else // background
-					{
-						// do nothing
-					}
 				}
 				else if(returnVal == 0 && current == 0) // child and background
 				{
 					int spoon = fork();
 					if(spoon == 0) 
 					{
-						execvp(args[0], (char * const *)args);
+						execvp(args[0], currentCommand.args);
 					}
 					else if(spoon > 0)
 					{
@@ -336,13 +374,21 @@ int main(int argc, char ** argv[])
 				}
 				else if(returnVal == 0)
 				{
-					execvp(args[0], (char * const *)args);
+					currentCommand.args[0] = strdup(args[0]);
+					for(int i = 0; i < 5; i++)
+					{
+						//printf("%s, ", currentCommand.args[i]);
+					}
+					printf("\n");
+					execvp(args[0], currentCommand.args);
+					perror("execvp\n");
+					exit(1);
 				}
 				else
 				{
 					printf ("Error\n");
 				}
-			}
+				}
 			
 		}
 		return 0;
