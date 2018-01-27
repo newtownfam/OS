e065e8d608b98e4c93658901e43daa7562571656
Andrew Morrison <andrew@Andrews-MacBook-Pro.local>
2018-01-27 17:25:56 -0500

    Attempted p2 phase 1

---
+++ P2/V1/ki1.c
@@ -0,0 +1,150 @@
+/* Andrew Morrison
+ * Peter Christakos */
+
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/syscalls.h>
+
+unsigned long **sys_call_table;
+
+/*~~~~~~~~~~~ Our code ~~~~~~~~~~~*/
+
+asmlinkage long (*ref_sys_open)(const char * file, int flags, mode_t mode);
+asmlinkage long (*ref_sys_close)(unsigned int fd);
+asmlinkage long (*ref_sys_read)(unsigned int fd, void * buf, size_t nbytes);
+
+asmlinkage long new_sys_open(const char * file, int flags, mode_t mode)
+{
+	int UID = current_UID().val();
+
+	if(UID>=1000)
+	{
+		printf("User %s is opening a file: %s\n", UID, file);
+	}
+	return ref_sys_open(file, flags, mode);
+}
+
+asmlinkage long new_sys_close(unsigned int file)
+{
+	int UID = current_UID().val();
+
+	if(UID>=1000)
+	{
+		printf("User %s is closing a file descriptor: %s\n", UID, file)
+	}
+	return ref_sys_close(file);
+}
+
+asmlinkage long new_sys_read(unsigned int fd, void * buf, size_t nbytes)
+{
+	int UID = current_UID().val();
+	off_t offset = 0;
+	ssize_t rv;
+
+	if(UID>=1000)
+	{
+		rv = ref_sys_read(fd, buf, nbytes);
+		if(rv != -1)
+		{
+			open((char *)buf, O_RDONLY);
+			if(strstr(buf, "VIRUS")==NULL)
+			{
+				printf("User %s read from file descriptor %d\n", UID, fd);
+			}
+			else
+			{
+				printf("User %s read from file descriptor %d, but that read contained malicious code!\n");
+			}
+		}
+	}
+	return ref_sys_read(fd, buf, nbytes);
+}
+
+
+/*~~~~~~~~~~~ Supplied Code ~~~~~~~~~~~*/
+
+static unsigned long **find_sys_call_table(void) {
+  unsigned long int offset = PAGE_OFFSET;
+  unsigned long **sct;
+  
+  while (offset < ULLONG_MAX) {
+    sct = (unsigned long **)offset;
+
+    if (sct[__NR_close] == (unsigned long *) sys_close) {
+      printk(KERN_INFO "Interceptor: Found syscall table at address: 0x%02lX",
+	     (unsigned long) sct);
+      return sct;
+    }
+    
+    offset += sizeof(void *);
+  }
+  
+  return NULL;
+}
+
+static void disable_page_protection(void) {
+  /*
+    Control Register 0 (cr0) governs how the CPU operates.
+
+    Bit #16, if set, prevents the CPU from writing to memory marked as
+    read only. Well, our system call table meets that description.
+    But, we can simply turn off this bit in cr0 to allow us to make
+    changes. We read in the current value of the register (32 or 64
+    bits wide), and AND that with a value where all bits are 0 except
+    the 16th bit (using a negation operation), causing the write_cr0
+    value to have the 16th bit cleared (with all other bits staying
+    the same. We will thus be able to write to the protected memory.
+
+    It's good to be the kernel!
+  */
+  write_cr0 (read_cr0 () & (~ 0x10000));
+}
+
+static void enable_page_protection(void) {
+  /*
+   See the above description for cr0. Here, we use an OR to set the 
+   16th bit to re-enable write protection on the CPU.
+  */
+  write_cr0 (read_cr0 () | 0x10000);
+}
+
+static int __init interceptor_start(void) {
+  /* Find the system call table */
+  if(!(sys_call_table = find_sys_call_table())) {
+    /* Well, that didn't work. 
+       Cancel the module loading step. */
+    return -1;
+  }
+  
+  /* Store a copy of all the existing functions */
+  ref_sys_cs3013_syscall1 = (void *)sys_call_table[__NR_cs3013_syscall1];
+
+  /* Replace the existing system calls */
+  disable_page_protection();
+
+  sys_call_table[__NR_cs3013_syscall1] = (unsigned long *)new_sys_cs3013_syscall1;
+  
+  enable_page_protection();
+  
+  /* And indicate the load was successful */
+  printk(KERN_INFO "Loaded interceptor!");
+
+  return 0;
+}
+
+static void __exit interceptor_end(void) {
+  /* If we don't know what the syscall table is, don't bother. */
+  if(!sys_call_table)
+    return;
+  
+  /* Revert all system calls to what they were before we began. */
+  disable_page_protection();
+  sys_call_table[__NR_cs3013_syscall1] = (unsigned long *)ref_sys_cs3013_syscall1;
+  enable_page_protection();
+
+  printk(KERN_INFO "Unloaded interceptor!");
+}
+
+MODULE_LICENSE("GPL");
+module_init(interceptor_start);
+module_exit(interceptor_end);
